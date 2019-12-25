f7f3efca3c35d91d540a9b8b15d7605114793a33
sha33415 <abidemi.shobayoeni@gmail.com>
2019-12-25 17:46:50 +0100

    Ceasar algorithm implemented. Test successfully passed

--- Basic_Text_Encryptor/Encryption/CeasarEncryption.cs
+++ Basic_Text_Encryptor/Encryption/CeasarEncryption.cs
@@ -9,20 +9,49 @@ namespace Basic_Text_Encryptor.Program
         private readonly int _key;
         public CeasarEncryption(int key)
         {
-            if(key <= 0 || key > 25)
+            if(key < 1 || key > 25)
             {
-                throw new ArgumentOutOfRangeException("Key", "Key for ceaser encryption must be between 1 and 25");
+                throw new ArgumentOutOfRangeException("Key", "Key for ceasar encryption must be between 1 and 25");
             }
             this._key = key;
         }
         public string Decrypt(string text)
         {
-            throw new NotImplementedException();
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < text.Length; i++)
+            {
+                char ch = text[i];
+                if (ch >= 'A' && ch <= 'Z')
+                {
+                    ch = Shift('A', ch, 26 - _key);
+                }
+                else if (ch >= 'a' && ch <= 'z')
+                {
+                    ch = Shift('a', ch, 26 - _key);
+                }
+                result.Append(ch);
+            }
+            return result.ToString();
         }
 
         public string Encrypt(string text)
         {
-            throw new NotImplementedException();
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < text.Length; i++)
+            {
+                char ch = text[i];
+                if (ch >= 'A' && ch <= 'Z')
+                {
+                    ch = Shift('A', ch, _key);
+                }
+                else if (ch >= 'a' && ch <= 'z')
+                {
+                    ch = Shift('a', ch, _key);
+                }
+                result.Append(ch);
+            }
+            return result.ToString();
         }
+        private char Shift(char offset, char text, int key) => (char)(offset + (text - offset + key) % 26);
     }
 }
